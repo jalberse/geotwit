a2a93b6d885794bb95700724c9e49324f42462f5
John Alberse <alberse.john@gmail.com>
2019-02-09 13:23:17 -0600

    basic stream set up

--- geotwit.py
+++ geotwit.py
@@ -1,21 +1,37 @@
-import tweepy
-import os
 import json
+import os
+
+import tweepy
+
+class MyStreamListenener(tweepy.StreamListener):
+    """
+    Listen for data
+    """
+    def on_status(self, status):
+        print(status.text) # TODO this
 
 if __name__ == '__main__':
     # Auth setup
     try:
-        if (os.path.isfile(".my_keys")):
-            keys_file = open(".my_keys",'r')
+        if os.path.isfile(".my_keys"):
+            KEYS_FILE = open(".my_keys", 'r')
         else:
-            keys_file = open("keys.json",'r')
-    except OSError as e:
+            KEYS_FILE = open("keys.json", 'r')
+    except OSError:
         print("Error opening keys file")
-    keys = json.load(keys_file)
-    consumer_key = keys['websites']['Twitter']['consumer_key']
-    consumer_secret = keys['websites']['Twitter']['consumer_secret']
-    access_token = keys['websites']['Twitter']['access_token']
-    access_token_secret = keys['websites']['Twitter']['access_secret']
 
-    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-    auth.set_access_token(access_token,access_token_secret)
+    KEYS = json.load(KEYS_FILE)
+    CONSUMER_KEY = KEYS['websites']['Twitter']['consumer_key']
+    CONSUMER_SECRET = KEYS['websites']['Twitter']['consumer_secret']
+    ACCESS_TOKEN = KEYS['websites']['Twitter']['access_token']
+    ACCESS_TOKEN_SECRET = KEYS['websites']['Twitter']['access_secret']
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    api = tweepy.API(auth_handler=auth)
+
+    myStreamListenener = MyStreamListenener()
+    stream = tweepy.Stream(auth=api.auth, listener=myStreamListenener)
+
+    stream.filter(track=['python'])
