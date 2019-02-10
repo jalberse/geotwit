076c0bd3330798d4aa87481ce668d20ec0e29e25
John Alberse <alberse.john@gmail.com>
2019-02-09 21:36:50 -0600

    update top 100 words getter

--- popular.py
+++ popular.py
@@ -13,22 +13,20 @@ def trending_words():
     """
     Get top 100 trending words from our data and saves in the topn.txt file
     """
-    data = pd.concat([pd.read_csv(f) for f in glob.glob('./data/*.csv')],ignore_index=True)
+    data = pd.concat([pd.read_csv(f) for f in glob.glob('./data/*.csv')],
+                     ignore_index=True)
 
     tokenizer = TweetTokenizer()
     vectorizer = TfidfVectorizer(tokenizer=tokenizer.tokenize)
-    response = vectorizer.fit_transform(data['Status'])
-    feature_names = vectorizer.get_feature_names()
+    response = vectorizer.fit_transform(data['Status'])    # Map feature index to feature name
+    feature_names = vectorizer.get_feature_names()    #  weight is the tf-idf value
     weight = vectorizer.idf_
-    feature_array = np.array(feature_names)
-    tfidf_sorting = np.argsort(response.toarray()).flatten()[::-1]
-    n = 100
-    top_n = feature_array[tfidf_sorting][:n]
-    #print(top_n)
-    #tfidf_scores = dict(zip(feature_names, weight))
-    #print(tfidf_scores)
+    #soring the weights
+    indices = np.argsort(weight)[::-1]
+    top_weights = 100
+    top_features = [feature_names[i] for i in indices[:top_weights]]
     with open('topn.txt', 'w') as f:
-        for word in top_n:
+        for word in top_features:
             f.write(word + '\n')
 
 if __name__ == '__main__':
